c9a2f3af8af5e54b9d3767ce8ab074698100e123
Glitch (glitch-hello-website) <none>
2023-01-10 08:08:34 +0000

    🎭🍎 Checkpoint ./index.html:53038234/17 ./script.js:53038234/3019

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function cariBarang(idInput, idUl) {
-  console.log("jalan")
-    var input, filter, ul, li, ahref, i, txtValue;
-    input = document.getElementById(idInput);
-    filter = input.value.toUpperCase();
-    ul = document.getElementById(idUl);
-    ahref = ul.getElementsByTagName("a");
-    for (i = 0; i < ahref.length; i++) {
-        // a = ;
-        txtValue = ahref[i].textContent || ahref[i].innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            ahref[i].style.display = "";
-        } else {
-            ahref[i].style.display = "none";
-        }
+  var input, filter, ul, li, ahref, i, txtValue;
+  input = document.getElementById(idInput);
+  filter = input.value.toUpperCase();
+  ul = document.getElementById(idUl);
+  ahref = ul.getElementsByTagName("a");
+  for (i = 0; i < ahref.length; i++) {
+    // a = ;
+    txtValue = ahref[i].textContent || ahref[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      ahref[i].style.display = "block";
+      console.log("muncul");
+    } else if (input.value == "") {
+      ahref[i].style.display = "none";
     }
+  }
 }
