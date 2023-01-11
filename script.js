9aa23211a09f2803da176e157e15a9f4e7ec3a07
Glitch (glitch-hello-website) <none>
2023-01-11 09:12:23 +0000

    ⛲️🏤 Checkpoint ./script.js:53038234/2938

--- script.js
+++ script.js
@@ -7,40 +7,46 @@ function cariBarang(idInput, idUl) {
     // a = ;
     txtValue = ahref[i].textContent || ahref[i].innerText;
     // console.log(input.value)
-    
+
     ahref[i].style.display = "none";
-    
+
     if (txtValue.toUpperCase().search(filter) > -1) {
       // console.log(txtValue)
       ahref[i].style.display = "block";
-    } 
-    
+    }
+
     if (input.value == "") {
       ahref[i].style.display = "none";
     }
   }
-  
-  ahref.forEach((e,n)=>{
-    e.onclick = (d)=>{
-      console.log(d.target.textContent)
-    }
-  })
+
+  ahref.forEach((e, n) => {
+    e.onclick = (d) => {
+      console.log(d.target.textContent);
+    };
+  });
 }
 
-function ambilDb(){
-  fetch("https://script.google.com/macros/s/AKfycbyRZXiLsIsTh0LzFk9z95tbHGArb3Ht03rLW_GgIv3F1kQmOlxkLjvNVPjcb3hjgh3NAg/exec", {
-    method : "post",
-    body: JSON.stringify({"perintah":"ambil db"})
-  })
-  .then(res => {
-    return res.json()
-  })
-  .then(resp => {
-    console.log(resp)
-    console.log(resp[2][0]) //kode barang
-    console.log(resp[2][1]) //nama barang
-  })
-  .catch(err => {
-    console.log(err)
-  })
+function ambilDb() {
+  fetch(
+    "https://script.google.com/macros/s/AKfycbyRZXiLsIsTh0LzFk9z95tbHGArb3Ht03rLW_GgIv3F1kQmOlxkLjvNVPjcb3hjgh3NAg/exec",
+    {
+      method: "post",
+      body: JSON.stringify({ perintah: "ambil db" }),
+    }
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((dataBarang) => {
+      // console.log(dataBarang)
+
+      for (let i = 2; i < dataBarang.length; i++) {
+        console.log(dataBarang[i][0]); //kode barang
+        console.log(dataBarang[i][1]); //nama barang
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
