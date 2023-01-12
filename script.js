82e772274c512316c24442110ae56858878328c3
Glitch (glitch-hello-website) <none>
2023-01-12 08:47:27 +0000

    🍝🎬 Checkpoint ./index.html:53038234/2003 ./script.js:53038234/1760

--- script.js
+++ script.js
@@ -23,11 +23,48 @@ function cariBarang(idInput, idUl) {
   ahref.forEach((e, n) => {
     e.onclick = (d) => {
       input.value = ""
-      console.log($(d.target).text())
+      
+      let templatelistba`<div class="input-group">
+          <input
+            id="nama_barang"
+            type="text"
+            aria-label="Nama barang"
+            class="form-control w-50"
+          readonly
+                 />
+          <input
+            id="kode_barang"
+            type="text"
+            aria-label="Kode barang"
+            class="form-control"
+          readonly
+                 />
+          <input
+            type="text"
+            aria-label="Satuan"
+            class="form-control"
+            readonly
+          />
+          <span class="btn btn-warning">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-trash-fill"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"
+              />
+            </svg>
+          </span>
+        </div>`
+      
       $("#kode_barang").val($(d.target).children("span").text())
+      $("#list_barang_masuk").prepend()
       let posisispan = ($(d.target).html()).search(" <span")
       let namaBarang = ($(d.target).html()).substring(0, posisispan)
-      console.log(namaBarang)
       $("#nama_barang").val(namaBarang)
       for (i = 0; i < ahref.length; i++) {
         ahref[i].style.display = "none";
