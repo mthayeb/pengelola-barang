88838d11f57b2ab1ba4f1740ce68ea13b10cad3f
Glitch (glitch-hello-website) <none>
2023-01-12 09:56:53 +0000

    💈🔔 Checkpoint ./script.js:53038234/5489

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let templateBarangMasuk = (namaBarang, kodeBarang) => {
+  return `
+    <tr>
+      <th scope="row"></th>
+      <td>${namaBarang}</td>
+      <td>${kodeBarang}</td>
+      <td><input type="text" class="form-control" placeholder="Jumlah" aria-label="Jumlah"></input></td>
+      <td><input type="text" class="form-control" placeholder="Satuan" aria-label="Satuan"></input></td>
+      <td><span class="btn btn-warning">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+          <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+        </svg>
+        </span>
+      </td>
+    </tr>`;
+      };
+
 function cariBarang(idInput, idUl) {
   var input, filter, ul, li, ahref, i, txtValue;
   input = document.getElementById(idInput);
@@ -24,34 +41,18 @@ function cariBarang(idInput, idUl) {
     e.onclick = (d) => {
       input.value = "";
 
-      let templateBarangMasuk = (namaBarang, kodeBarang) => {
-        let panjangList = $(list_barang_masuk).children().length
-        $("#list_barang_masuk th").html("")
-        
-        let noUrut = $("#list_barang_masuk th").eq().html("")
-        
-  return `
-    <tr>
-      <th scope="row">${noUrut}</th>
-      <td>${namaBarang}</td>
-      <td>${kodeBarang}</td>
-      <td><input type="text" class="form-control" placeholder="Jumlah" aria-label="Jumlah"></input></td>
-      <td><input type="text" class="form-control" placeholder="Satuan" aria-label="Satuan"></input></td>
-      <td><span class="btn btn-warning">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-      <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-    </svg>
-    </span></td>
-    </tr>`;
-      };
-
       let posisispan = $(d.target).html().search(" <span");
       let namaBarang = $(d.target).html().substring(0, posisispan);
-
+        
       $("#list_barang_masuk").prepend(
         templateBarangMasuk(namaBarang, $(d.target).children("span").text())
       );
-
+      
+      let panjangList = $(list_barang_masuk).children().length
+      for(let i = 1; i < panjangList;i++){
+        $("#list_barang_masuk th").eq(i - 1).html(i)  
+      }
+      
       for (i = 0; i < ahref.length; i++) {
         ahref[i].style.display = "none";
       }
